d862dcccdea0313725eab3b968749b3ad94a96db
SUJOY RAY <sujoygithub@gmail.com>
2025-12-03 15:53:24 +0530

    organized the project

--- app/main.py
+++ app/main.py
@@ -2,7 +2,6 @@ from fastapi import FastAPI, Depends, File, Request, Form, UploadFile, WebSocket
 from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
 from sqlalchemy.orm import Session
-from ipconverter import ip_to_location
 from database import Query, SessionLocal, init_db
 from crud import create_user, get_user_by_token, save_query, chat_history
 from sentiment import analyze_sentiment
@@ -11,7 +10,6 @@ import os
 init_db()
 app = FastAPI()
 templates = Jinja2Templates(directory="templates")
-INACTIVITY_TIMEOUT = 2.0
 
 # Dependency
 def get_db():
@@ -25,12 +23,11 @@ def get_db():
 
 @app.get("/register", response_class=HTMLResponse)
 def register_form(request: Request):
-    return templates.TemplateResponse("register.html", {"request": request})
-
+    return templates.TemplateResponse("auth/register.html", {"request": request})
 
 @app.post("/register", response_class=HTMLResponse)
 def register_user(request: Request, username: str = Form(...), db: Session = Depends(get_db)):
-    client_host = request.client.host  # get IP address
+    client_host = request.client.host
     db_user = create_user(db, username, ip_address=client_host)
     return templates.TemplateResponse("welcome.html", {
         "request": request,
@@ -38,12 +35,9 @@ def register_user(request: Request, username: str = Form(...), db: Session = Dep
         "session_token": db_user.session_token
     })
 
-# ---------- Sentiment ----------
-
-
 @app.get("/sentiment-form", response_class=HTMLResponse)
 def sentiment_form(request: Request):
-    return templates.TemplateResponse("sentiment_form.html", {
+    return templates.TemplateResponse("/sentiment/sentiment_form.html", {
         "request": request,
         "text": "",
         "sentiment": None,
@@ -53,7 +47,7 @@ def sentiment_form(request: Request):
 
 
 def render_error(request: Request, message: str):
-    return templates.TemplateResponse("sentiment_form.html", {
+    return templates.TemplateResponse("component/error.html", {
         "request": request,
         "error": message
     })
@@ -80,11 +74,11 @@ def sentiment_analysis(request: Request, text: str = Form(...), token: str = For
         "error": None
     }
     print(data)
-    return templates.TemplateResponse("sentiment_form.html", data)
+    return templates.TemplateResponse("sentiment/sentiment_form.html", data)
 
 @app.get("/map")
 def map_page(request: Request):
-    return templates.TemplateResponse("sentiment_map.html", {"request": request})
+    return templates.TemplateResponse("sentiment/sentiment_map.html", {"request": request})
 
 
 @app.get("/sentiment-map")
